6532f8ad7df988933edb2ba3efd6a67caca944eb
JanaWalfort <JWalfort@tobit.ag>
2020-08-25 17:35:53 +0200

    :construction: working on Milestone 2

--- src/index.js
+++ src/index.js
@@ -1,27 +1,33 @@
+/* eslint-disable no-console */
 import './app.scss';
-
-
-// init(); npm install whatwg-fetch --save
 
 chayns.ui.initAll();
 
 // const url = 'https://chayns1.tobit.com/TappApi/Site/SlitteApp?SearchString=love&Skip=0&Take=50';
 
-//fetch('https://chayns1.tobit.com/TappApi/Site/SlitteApp?SearchString=love&Skip=0&Take=50')
-   // .then(resp => resp.json())
-   // .then(data => getData(data.Data));
+fetch('https://chayns1.tobit.com/TappApi/Site/SlitteApp?SearchString=love&Skip=0&Take=50')
+   .then(resp => resp.json())
+   .then(data => getData(data.Data));
 
-//function getData(data) {
-    //data.forEach(element => {
-        // const siteId = element.siteId;
-        // const name = element.appstoreName;
-        // const sites = document.createElement('div');
-        // const siteName = document.createElement("P");
-        // siteName.innerText = name;
-        // const pic = document.createElement('img');
-        // pic.src = `https://chayns.tobit.com/storage/${siteId}/Images/icon-57.png`;
-        // document.getElementById("site-name").appendChild(siteName);
-        // document.getElementById("site").appendChild(pic);
-       // console.log('loading');
-    //});
-//}
+function getData(data) {
+    const element = data;
+    // eslint-disable-next-line no-plusplus
+    for (let i = 0; i < data.length; i++) {
+        const fav = document.createElement('div');
+        fav.classList.add('site');
+        const $sites = document.querySelector('.sites');
+        $sites.appendChild(fav);
+
+        const pic = document.createElement('div');
+        const { locationId } = element[i];
+        console.log(locationId);
+        pic.style = `background-image: url(https://sub60.tobit.com/l/${locationId}?size=57); width: 57px; height: 57px`;
+        fav.appendChild(pic);
+
+        const name = element[i].appstoreName.substring(0,12);
+        console.log(name);
+        const p = document.createElement('P');
+        p.innerText = name;
+        fav.appendChild(p);
+     }
+}
